c21d133c9386019a72649d756dcfdc09c473d6f0
Brandt-C <brandt.carlson11@gmail.com>
2022-03-11 16:44:00 -0600

    navbar in

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 from config import Config
 
-app = Flask(__name__, static_url_path='/static', static_folder='static')
+app = Flask(__name__)
 
 app.config.from_object(Config)
 
